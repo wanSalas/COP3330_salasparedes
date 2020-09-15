6a486d52dcff77e453c3382e075c17a088be32a0
wanSalas <70238165+wanSalas@users.noreply.github.com>
2020-09-15 00:58:56 -0400

    test

--- salasparedes_p1/src/Decrypter.java
+++ salasparedes_p1/src/Decrypter.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class Decrypter {
         public void decrypt(String fourNumbers)
--- salasparedes_p1/src/Encrypter.java
+++ salasparedes_p1/src/Encrypter.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class Encrypter {
     public void encrypt(String fourNumbers)
