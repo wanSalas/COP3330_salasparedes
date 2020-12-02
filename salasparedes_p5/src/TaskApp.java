47f00d506ab7bbe3b2a28feb61e32f90b6076d37
wanSalas <70238165+wanSalas@users.noreply.github.com>
2020-12-01 20:15:30 -0500

    jk its now the final Commit made a constructor for taskApp

--- salasparedes_p5/src/TaskApp.java
+++ salasparedes_p5/src/TaskApp.java
@@ -4,7 +4,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TaskApp {
-    public static void main(String[] args){
+    public TaskApp(){
         mainMenu();
     }
     private static void mainMenu(){
